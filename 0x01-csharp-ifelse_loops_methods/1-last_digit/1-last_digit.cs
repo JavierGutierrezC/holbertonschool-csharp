7f225b9a12b90df7093830d81c4d3129b1f0e7a8
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 08:20:25 -0500

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastd = number % 10;
+        
+        if (lastd > 5)
+        {
+        Console.WriteLine("The last digit of " + number + " is " + lastd + " and is greater than 5");
+        }
+        else if (lastd == 0)
+        {
+        Console.WriteLine("The last digit of " + number + " is " + lastd + " and is 0");
+        }
+        else
+        {
+        Console.WriteLine("The last digit of " + number + " is " + lastd + " and is less than 6 and not 0");
+        }
+    }
+}

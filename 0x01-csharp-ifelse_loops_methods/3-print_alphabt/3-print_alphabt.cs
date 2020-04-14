9a92a26296068a4abfa5b95fe9bec99490b37b65
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 08:40:16 -0500

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alpha;
+            for (alpha = 'a'; alpha < 'z'; alpha++)
+            {
+                if(alpha == 'e' || alpha == 'q')
+                {
+                continue;
+                }
+            Console.Write(alpha);
+                
+            }
+        }
+    }
+}

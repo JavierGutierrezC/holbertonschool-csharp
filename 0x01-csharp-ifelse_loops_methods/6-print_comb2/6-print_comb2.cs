f38dc1b7df9d5b651ece1def82a7a633370b3944
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 09:59:01 -0500

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string coma= "";
+        int y;
+        int z;
+        for (y = 0; y <= 9; y++)
+            { 
+            for (z = y + 1; z <= 9; z++)
+                {
+                Console.Write("{0}{1:d}{2:d}",coma, y, z);
+                coma = ", ";
+                } 
+            }
+        Console.Write("\n");
+        }
+    }
+}

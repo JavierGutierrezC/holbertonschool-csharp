48d3b779e5b0b6f083550e32c0469b453fd5642d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 08:28:52 -0500

    [Fix]change double quotes for single

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alpha;
+            for (alpha = 'a'; alpha < 'z'; alpha++)
+            Console.Write(alpha);
+        }
+    }
+}

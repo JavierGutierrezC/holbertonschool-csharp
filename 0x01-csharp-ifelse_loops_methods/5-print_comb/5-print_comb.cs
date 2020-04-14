7447c6c5707f210ed7fcaa90fff0a9b72f58e5a9
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 09:17:36 -0500

    [Fix]coma variable after Write

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string coma= "";
+        int y;
+        for (y = 0; y <= 99; y++)
+            { 
+            Console.Write("{0}{1:d2}",coma, y);
+            coma = ", "; 
+            }
+        Console.Write("\n");
+        }
+    }
+}

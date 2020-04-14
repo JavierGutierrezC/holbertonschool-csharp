c6461f2bcacb9571e8464c4499bfc7281dce1e9a
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 11:05:49 -0500

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            int x = 0;
+            if (length > 0)
+            {
+                for (x = 0; x <= length; x++)
+                {
+                    Console.Write("_");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n");
+            }
+        }
+    }

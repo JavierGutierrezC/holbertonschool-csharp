7b70f425c5c0a442d54a6ab5a28f4327725af676
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 11:39:14 -0500

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            int x;
+            int y;
+            if (length > 0)
+            {
+                for (x = 0; x < length; x++)
+                {
+                    //Console.Write(x);
+                    for (y = 0; y < x; y++)
+                    {
+                        //Console.Write(y);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("\\");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }

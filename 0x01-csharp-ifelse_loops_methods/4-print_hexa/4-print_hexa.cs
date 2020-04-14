cc2fd8b30de8a7e207998fa006ea5065e0657bf2
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-14 08:54:34 -0500

    with WriteLine

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            for (y = 0; y < 99; y++)
+            { 
+            Console.WriteLine("{0:d} = 0x{0:x}",y);   
+            }
+        }
+    }
+}

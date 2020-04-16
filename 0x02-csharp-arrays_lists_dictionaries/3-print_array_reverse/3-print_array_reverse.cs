110b705e412ff1cfa9d50e5059c0d49749fd1ba0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 12:52:48 -0500

    [Fix]add second Console.Write

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            { 
+                Console.WriteLine();
+            }
+            else
+            {
+                int arrayLength = array.Length -1;
+                string space = " ";
+                for (int x = arrayLength; x >= 0; x--)
+                { 
+                    Console.Write(array[x]);
+                    Console.Write(space);
+                }
+            }
+        }
+    }

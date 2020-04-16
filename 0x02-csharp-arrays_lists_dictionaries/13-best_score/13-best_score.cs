b5b4ae1ed8f2ec6a357849da68d2a88cd2113c1d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 18:43:13 -0500

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            string name = "0";
+            foreach (KeyValuePair<string, int> elem in myList.OrderByDescending(key => key.Value))
+            {
+                //Console.WriteLine("{0}: {1}", elem.Key, elem.Value);
+                name = elem.Key;
+                return(name);
+            }
+            return(name);
+        }
+    }

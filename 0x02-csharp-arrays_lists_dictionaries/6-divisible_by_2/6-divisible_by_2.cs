738a466a584cac808815f7f03ed0220c138aed5e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 15:48:03 -0500

    [Fix]Change list to List

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> byTwo = new List<bool>();
+            for (int x = 0; x < myList.Count; x++)
+            {
+                if (myList[x] % 2 == 0)
+                {
+                    byTwo.Add(true);
+                }
+                else
+                {
+                    byTwo.Add(false);
+                }
+            //return(byTwo);
+            }
+            return(byTwo);
+        }
+    }

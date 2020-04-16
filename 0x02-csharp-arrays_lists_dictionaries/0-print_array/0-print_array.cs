9db8bf52bd25da92aff04782d2b90262bfdad927
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 10:38:19 -0500

    [Fix]use.Join

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            { 
+                Console.WriteLine("Size cannot be negative");
+                return(null);
+             }
+            else
+            {
+                int[] myArray = new int[size];
+                int x;
+                for (x = 0; x < size; x++)
+                {
+                    myArray[x] = x; 
+                    //Console.Write(" " + myArray[x]);
+                }
+                Console.WriteLine(string.Join(" ",myArray));
+                return(myArray);
+
+            }
+        }
+    }

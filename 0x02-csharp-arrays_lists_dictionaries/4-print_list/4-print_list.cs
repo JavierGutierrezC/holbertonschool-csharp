9dad8e80980e4873750b7038ccdd3ef82ceccdce
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 14:42:19 -0500

    [Fix]Add instead of add.Join inlude separator

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if(size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return(null);
+            }
+            else
+            {
+                List<int> myList = new List<int>();
+                for (int x = 0; x < size; x++)
+                {
+                    myList.Add(x);
+                    //Console.Write(myList[x]);
+                    
+                }
+            Console.WriteLine(string.Join(" ", myList));
+            return(myList);
+            }
+        }
+    }
+

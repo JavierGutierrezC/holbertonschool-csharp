7870909c4cdc80894ecf4c5adbba20df486c9fe7
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 18:49:07 -0500

    [Fix]added none condiiton

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,11 +6,18 @@ using System.Linq;
         public static string BestScore(Dictionary<string, int> myList)
         {
             string name = "0";
-            foreach (KeyValuePair<string, int> elem in myList.OrderByDescending(key => key.Value))
+            if(myList.Count == 0)
             {
-                //Console.WriteLine("{0}: {1}", elem.Key, elem.Value);
-                name = elem.Key;
-                return(name);
+                return("None");
+            }
+            else
+            {
+            foreach (KeyValuePair<string, int> elem in myList.OrderByDescending(key => key.Value))
+                {
+                    //Console.WriteLine("{0}: {1}", elem.Key, elem.Value);
+                    name = elem.Key;
+                    return(name);
+                }
             }
             return(name);
         }

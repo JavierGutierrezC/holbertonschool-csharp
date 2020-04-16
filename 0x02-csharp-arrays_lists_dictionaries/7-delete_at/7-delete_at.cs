611d56adc6c3328d4f1e4a62c41404461cbc2264
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 16:15:58 -0500

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DeleteAt(List<int> myList, int index)
+        {
+            if(index < 0 || index >= myList.Count)
+            {
+                Console.WriteLine("Index is out of range");
+                return(myList);
+            }
+            else
+            {
+                myList.Remove(index);
+                //Console.WriteLine(index);
+                return(myList);
+            }   
+            //return(myList);
+        }
+    }

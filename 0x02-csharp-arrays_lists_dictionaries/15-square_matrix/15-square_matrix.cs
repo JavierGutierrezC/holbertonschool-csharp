7973175110dd955b43b8dfba4f099146b83e1850
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 20:44:50 -0500

    [Fix]add 1 to second getlength

--- 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -5,10 +5,10 @@
     {
         public static int[,] Square(int[,] myMatrix)
         {
-            int[,] newM = new int[myMatrix.GetLength(0),myMatrix.GetLength(0)];
+            int[,] newM = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
             for (int x = 0; x < myMatrix.GetLength(0); x++)
             {
-                for (int y = 0; y < myMatrix.GetLength(0); y++)
+                for (int y = 0; y < myMatrix.GetLength(1); y++)
                 {
                     newM[x,y] = (int)Math.Pow(myMatrix[x,y], 2);
                     //Console.Write(newM[x,y]);

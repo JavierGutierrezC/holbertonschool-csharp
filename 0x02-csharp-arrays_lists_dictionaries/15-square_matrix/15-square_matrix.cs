b444305585f2eed92bd0c371e3959d24015c8f4a
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 20:41:36 -0500

    [Fix]cast to int

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+            int[,] newM = new int[myMatrix.GetLength(0),myMatrix.GetLength(0)];
+            for (int x = 0; x < myMatrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < myMatrix.GetLength(0); y++)
+                {
+                    newM[x,y] = (int)Math.Pow(myMatrix[x,y], 2);
+                    //Console.Write(newM[x,y]);
+                }
+                
+            } 
+            return(newM);
+        }
+    }

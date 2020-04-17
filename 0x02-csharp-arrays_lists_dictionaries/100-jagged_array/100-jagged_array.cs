3589ed728c8e5e44e223593585dc7a26c29c6689
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 21:15:44 -0500

    [Fix]condition for white space

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -5,17 +5,21 @@
     {
         static void Main(string[] args)
         {
-            int[][] ja_Arr = new int[3][];
+            int[][] ja_Arr = new int[][]
             {
-                ja_Arr[0] = new int[] {0, 1, 2, 3};
-                ja_Arr[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
-                ja_Arr[2] = new int[] {0, 1};
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
             };
             for (int x = 0; x < ja_Arr.Length; x++) 
             {
                 for (int y = 0; y < ja_Arr[x].Length; y++) 
                 {
                     Console.Write(ja_Arr[x][y]);
+                    if(y !=ja_Arr[x].Length -1)
+                    {
+                        Console.Write(" ");
+                    }
                 }
             Console.WriteLine();
             }

cf9d3b9d0f810ae6a611e8f0e3c22e81c591d995
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 19:28:14 -0500

    add condition last space

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -10,7 +10,14 @@
             {
                 for (int y = 0; y < 5; y++)
                 {
-                Console.Write(array[x,y] + " ");
+                    if (y < 4)
+                    {
+                        Console.Write(array[x,y] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(array[x,y]);
+                    }
                 }
             Console.WriteLine();
             }

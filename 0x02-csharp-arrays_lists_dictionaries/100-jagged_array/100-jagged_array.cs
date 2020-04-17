77d6ad4ffea034b5a70273a83f0866ac32a096a0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 21:08:52 -0500

    [Fix]second for index

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ja_Arr = new int[3][];
+            {
+                ja_Arr[0] = new int[] {0, 1, 2, 3};
+                ja_Arr[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+                ja_Arr[2] = new int[] {0, 1};
+            };
+            for (int x = 0; x < ja_Arr.Length; x++) 
+            {
+                for (int y = 0; y < ja_Arr[x].Length; y++) 
+                {
+                    Console.Write(ja_Arr[x][y]);
+                }
+            Console.WriteLine();
+            }
+            
+        }
+    }

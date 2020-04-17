73fdc1e4659d3130116d98b17c17034db5cbf0df
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-16 19:16:55 -0500

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            array[2,2] = 1;
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                Console.Write(array[x,y] + " ");
+                }
+            Console.WriteLine();
+            }
+        }  
+    }

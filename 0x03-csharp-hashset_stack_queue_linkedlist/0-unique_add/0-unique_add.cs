84ec43db04ab99fa6088e1cf0f5802cf997a3435
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 11:48:18 -0500

    [Fix]eliminate ; after foreach loop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            HashSet<int> myHash = new HashSet<int>(myList);
+            int count = 0;
+
+            foreach(int item in myHash)
+            {
+                count += item;
+            }
+            return(count);
+        }
+    }

d51ec2b6a5ad497349936798c5e3695045a2c2da
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 17:44:44 -0500

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            int ind = 0;
+            foreach(int item in myLList)
+            {
+                if(ind == n)
+                {
+                    return(item);
+                }
+                ind++;
+            } 
+            return(0);
+        }
+    }
+

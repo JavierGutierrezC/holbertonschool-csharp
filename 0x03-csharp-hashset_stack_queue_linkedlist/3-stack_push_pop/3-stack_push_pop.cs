5253e22a88f1941103b6e756b3c7c775837db6b2
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 13:35:38 -0500

    first commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class MyStack
+    {
+        public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+        {
+            Console.WriteLine("Number of items: {0}", aStack.Count);
+            if(aStack.Count > 0)
+                {
+                    Console.WriteLine("Top item: {0}", aStack.Peek());
+                }
+            else
+                {
+                    Console.WriteLine("Stack is empty");
+                }
+            if(aStack.Contains(search))
+                {
+                    Console.WriteLine("Stack contains {0}: True", search);
+                    while(aStack.Peek() != search)
+                    aStack.Pop();
+                    if(aStack.Peek() == search)
+                    {
+                        aStack.Pop();
+                    }
+                }
+            else
+                {
+                    Console.WriteLine("Stack contains {0}: False", search);
+                }
+            aStack.Push(newItem);
+            return(aStack);
+        }
+        
+    }
+

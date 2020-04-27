e605323d36733532c57b55aa7dee906083a36b0d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 14:06:39 -0500

    [Fix]Remove 2nd Pop

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -17,12 +17,8 @@ using System.Collections.Generic;
             if(aStack.Contains(search))
                 {
                     Console.WriteLine("Stack contains \"{0}\": True", search);
-                    while(aStack.Peek() != search)
+                    while((aStack.Peek() != search || aStack.Peek() == search) && aStack.Contains(search))
                     aStack.Pop();
-                    if(aStack.Peek() == search)
-                    {
-                        aStack.Pop();
-                    }
                 }
             else
                 {

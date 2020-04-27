2dd8d08ed92a404d3ae277970c8409054dd8c9d3
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 15:10:59 -0500

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class MyQueue
+    {
+        public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+        {
+            Console.WriteLine("Number of items: {0}", aQueue.Count);
+            if(aQueue.Count > 0)
+                {
+                    Console.WriteLine("First item: {0}", aQueue.Peek());
+                }
+            else
+                {
+                    Console.WriteLine("Queue is empty");
+                }
+            if(aQueue.Contains(search))
+                {
+                    Console.WriteLine("Queue contains \"{0}\": True", search);
+                    while((aQueue.Peek() != search || aQueue.Peek() == search) && aQueue.Contains(search))
+                    aQueue.Dequeue();
+                }
+            else
+                {
+                    Console.WriteLine("Queue contains \"{0}\": False", search);
+                }
+            aQueue.Enqueue(newItem);
+            return(aQueue);
+        }
+        
+    }
+

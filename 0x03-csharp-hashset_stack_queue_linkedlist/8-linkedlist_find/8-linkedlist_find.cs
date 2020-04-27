c83bab66f6eaa2e03d69791a61a29399cee87c45
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 17:04:14 -0500

    [Fix]Delete an else

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int FindNode(LinkedList<int> myLList, int value)
+        {
+            int ind = 0;
+            foreach(int item in myLList)
+            {
+                if(item == value)
+                {
+                    return(ind);
+                }
+                ind++;
+            } 
+            return(-1);
+        }
+    }

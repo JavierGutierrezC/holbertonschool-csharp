2a2b2144ba8d5f9866d35ac3d31ff034831a3dfd
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 12:27:35 -0500

    task 1 and 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> list3 = list1.FindAll(num => list2.Contains(num));
+        return(list3);
+        }
+        
+    }
+
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            List<int> sorted1 = list1.FindAll(num => !list2.Contains(num));
+            //foreach(int nu in sorted1)
+            //Console.Write($"{nu}");
+            List<int> sorted2 = list2.FindAll(num => !list1.Contains(num));
+            //foreach(int ni in sorted2)
+            //Console.Write($"{ni}");
+            sorted1.AddRange(sorted2);
+            //foreach(int ni in sorted1)
+            //Console.Write($"{ni}");
+            sorted1.Sort();
+        return(sorted1);
+        }
+        
+    }
+

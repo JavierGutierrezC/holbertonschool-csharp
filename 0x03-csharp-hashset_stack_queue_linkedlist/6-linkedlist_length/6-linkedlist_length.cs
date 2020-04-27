d2ea07de1e8fb7ab520077aadcb1280e08b09c2d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 16:14:49 -0500

    [Fix]Indentation

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int Length(LinkedList<int> myLList)
+        {
+            int Cont = 0;
+            foreach(int item in myLList)
+                Cont++;
+
+            return(Cont);
+        }
+    }

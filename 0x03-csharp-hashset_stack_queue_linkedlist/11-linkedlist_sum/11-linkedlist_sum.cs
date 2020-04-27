ba049f83ea98a97eab28b5cec051a3405f87d4fb
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 17:10:46 -0500

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int Sum(LinkedList<int> myLList)
+        {
+            int Cont = 0;
+            foreach(int item in myLList)
+                Cont += item;
+
+            return(Cont);
+        }
+    }
+

90bd496d95b4be88c8e6d05535a4316500c46e82
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 21:19:28 -0500

    [Fix]Add return

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+        {
+            //LinkedListNode<int> node = new LinkedListNode<int>(n);
+            LinkedListNode<int> act = myLList.First;
+            while(act != null)
+            {
+                if(act.Value < n)
+                {
+                    act = act.Next;
+                }
+                else
+                {
+                    return(myLList.AddBefore(act, n));
+                    break;
+                }   
+            }
+            return(myLList.AddLast(n));  
+        }
+    }
+

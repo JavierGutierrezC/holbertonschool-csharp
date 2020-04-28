e5ba40339daabbcf173ade89121543be7b1f0c59
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-28 10:06:56 -0500

    First commit

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            
+            int count = 0;
+           try
+            {
+                for(int var = 0; var < n; var++)
+                {
+                    Console.WriteLine(myList[var]);
+                    count++;
+                }
+                return(count);
+            }
+            catch
+            {
+                return(count);
+            }
+        }
+    }
+
---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int x = 0;
+            try
+            {
+                x = a / b;
+                //Console.WriteLine(x);
+            }
+            catch
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine("{0} / {1} = {2}",a, b, x);
+            }
+        }
+    }
+

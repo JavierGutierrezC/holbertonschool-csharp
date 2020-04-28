6b18e7892e3db54360b16cd9f1d766a3362cf57c
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-28 14:13:47 -0500

    [Fix]change variable names so they are not the same

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    public enum Rating {Good, Great, Excellent,}
+    public struct Dog
+    {
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    
+
+        public Dog(string lname, float lage, string lowner, Rating lrating)
+        {
+            name = lname;
+            age = lage;
+            owner = lowner;
+            rating = lrating;
+        }
+    }

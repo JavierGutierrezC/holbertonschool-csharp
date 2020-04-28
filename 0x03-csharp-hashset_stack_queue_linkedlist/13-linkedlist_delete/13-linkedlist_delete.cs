c8c2921d5f08d8cca727efaf972b5f179e39e773
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-27 20:06:31 -0500

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static void Delete(LinkedList<int> myLList, int index)
+        {
+            
+            LinkedListNode<int> act = myLList.First;
+          
+        
+            for(int x = 0; x < index; x++) 
+            {
+                //Console.WriteLine("indice {0}", x); 
+                //Console.WriteLine("indice.val1 {0}", act.Value);  
+                act = act.Next;
+                //Console.WriteLine("indice.val2 {0}", act.Value);
+            }
+            myLList.Remove(act);
+            
+        }
+    }

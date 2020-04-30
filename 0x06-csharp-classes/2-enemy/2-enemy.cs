16f5e6d9390a981ccbd15a480a1b168c319f5964
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-30 13:29:53 -0500

    [Fix]Added new to the throw

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>health public field</summary>
+        public int health;
+        ///<summary>Zombie public constructor</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Zombie public constructor that accepts a value</summary>
+        public Zombie(int value)
+        {
+            
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+    }
+}
+

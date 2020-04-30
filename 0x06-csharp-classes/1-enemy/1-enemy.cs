7d6fa49ad1d3f790f17007a53a9050489f7c3274
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-30 13:14:28 -0500

    First commit

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>health public field</summary>
+        public int health;
+        ///<summary>Zombie public constructor</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}

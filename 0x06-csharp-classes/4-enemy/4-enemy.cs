ac3a06274aff092c7190e516b0ab0a0612665203
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-30 14:53:06 -0500

    [Fix]set = value

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>health public field</summary>
+        private int health;
+        ///<summary>Zombie public constructor</summary>
+        public Zombie()
+        {
+             health = 0;
+        }
+        ///<summary>Zombie public constructor that accepts a value</summary>
+        public Zombie(int value)
+        {
+            
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+        ///<summary>Private field</summary>
+        private string name = "(No name)";
+        ///<summary>Public property Name</summary>
+        public string Name
+        {
+            get 
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+        ///<summary>GetHealth public constructor</summary>
+        public int GetHealth()
+        {
+            return(health);
+        }
+
+    }
+}

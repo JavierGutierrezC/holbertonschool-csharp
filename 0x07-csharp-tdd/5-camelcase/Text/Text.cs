3652774da5c02d9bd9826898c304a41f3e64a058
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-05 20:52:06 -0500

    First commit

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+     /// <summary>Str Class</summary>
+    public class Str
+    {
+        /// <summary>Find number of words</summary>
+        /// <param name="s">String of words.</param>
+        /// <returns> number of words</returns>
+        public static int CamelCase(string s)
+        {
+            if(s == "" || s == null)
+            {
+                return(0);
+            }
+            int cont = 1;
+            for(int x = 0; x < s.Length; x++)
+            {
+                if(char.IsUpper(s[x]) == true)
+                {
+                    cont ++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return(cont);
+        }    
+    }
+}

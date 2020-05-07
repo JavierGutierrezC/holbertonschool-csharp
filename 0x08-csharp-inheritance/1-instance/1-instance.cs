5c42c482ac5eb95c8af1ffe29469e61e75202d17
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-07 11:30:45 -0500

    [Fix]Add comments

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+    ///<summary>Obj class</summary>
     class Obj
     {
+        /// <summary> method that returns True 
+        ///if the object is an instance of, or if the object is an instance of 
+        ///a class that inherited from, Array</summary>
         public static bool IsInstanceOfArray(object obj)
         {
             if(obj is Array)

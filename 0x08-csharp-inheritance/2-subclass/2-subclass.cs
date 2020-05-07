8a40062a8282b192daebea2a68a65ed7bf6c227c
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-07 11:37:14 -0500

    first commit

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    
+    ///<summary>Obj class</summary>
+    class Obj
+    {
+        ///<summary> returns True if the object is an instance of a class 
+        ///that inherits from the specified class</summary>
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return(derivedType.IsSubclassOf(baseType));
+        } 
+    }

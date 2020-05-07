d9f033fcdcb8063221a474785e1ceb7711831967
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-07 11:16:13 -0500

    First commit

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Obj
+    {
+        public static bool IsInstanceOfArray(object obj)
+        {
+            if(obj is Array)
+            {
+                return(true);
+            }
+            else
+            {
+                return(false);
+            }
+        }
+    }

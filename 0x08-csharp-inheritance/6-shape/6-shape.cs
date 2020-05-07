bb28dc3259668cea2375951dc76edad0d75edb33
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-07 12:46:14 -0500

    [Fix]if condition

--- 0x08-csharp-inheritance/6-shape/6-shape.cs
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -24,7 +24,7 @@
             }
             set
             {
-                if(value == -0)
+                if(value < 0)
                 {
                    throw new ArgumentException("Width must be greater than or equal to 0"); 
                 }
@@ -42,7 +42,7 @@
             }
             set
             {
-                if(value == -0)
+                if(value < 0)
                 {
                    throw new ArgumentException("Height must be greater than or equal to 0"); 
                 }

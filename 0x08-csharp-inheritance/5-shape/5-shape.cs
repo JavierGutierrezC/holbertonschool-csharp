23945822357b9ad5769223596061fbb2f14b18f7
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-07 12:24:09 -0500

    First commit

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    ///<summary>Shape class</summary>
+    class Shape
+    {
+        ///<summary>Public Method</summary>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");       
+        }
+    }

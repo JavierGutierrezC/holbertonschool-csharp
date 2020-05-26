034a8e16c2529f14deec6373d2229c41c81e61fa
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-26 18:12:37 -0500

    [Fix]erase vector 1 and 2 from prior exercise

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+///<summary>Class VectorMath</summary>
+    class VectorMath
+    {
+        ///<summary>adds two vectors and returns the resulting vector</summary>
+        ///<return>new vector with result</return>
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+            if (vector.Length == 2 && vector.Length == 2 )
+            {
+                double[] two = new double[] {0, 0};
+                two[0] = vector[0] * scalar;
+                two[1] = vector[1] * scalar;
+                return (two);
+            }
+            if (vector.Length == 3 && vector.Length == 3)
+            {
+                double[] three = new double[] {0, 0, 0};
+                three[0] = vector[0] * scalar;
+                three[1] = vector[1] * scalar;
+                three[2] = vector[2] * scalar;
+                return (three);       
+            }
+            else
+            {
+                double[] none = new double[] {-1};
+                return(none);
+            }
+        }
+    }

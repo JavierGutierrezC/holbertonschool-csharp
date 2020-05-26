ed7867f5727a541decc01989c8dce3deeb4d61b4
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-26 18:50:01 -0500

    [Fix]doubles and ints

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+///<summary>Class VectorMath</summary>
+    class VectorMath
+    {
+        ///<summary>adds two vectors and returns the resulting vector</summary>
+        ///<return>new vector with result</return>
+        public static double DotProduct(double[] vector1, double[] vector2)
+        {
+            if (vector1.Length == 2 && vector2.Length == 2)
+            {
+                double two = vector1[0] * vector2[0];
+                double two1 = vector1[1] * vector2[1];
+                double ans = two + two1;
+                return (ans);
+            }
+            if (vector1.Length == 3 && vector2.Length == 3)
+            {
+                double three = vector1[0] * vector2[0];
+                double three1 = vector1[1] * vector2[1];
+                double three2 = vector1[2] * vector2[2];
+                double ans = three + three1 + three2;
+                return (ans);       
+            }
+            else
+            {
+                return(-1);
+            }
+        }
+    }

c11c9edcb38c73f7e18bfb5473ecd4ee5475c0d3
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-26 17:29:21 -0500

    [Fix]return in else

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+///<summary>Class VectorMath</summary>
+    class VectorMath
+    {
+        
+        ///<summary>adds two vectors and returns the resulting vector</summary>
+        ///<return>length or -1</return>
+        public static double[] Add(double[] vector1, double[] vector2)
+        {
+            if (vector1.Length == 2 && vector2.Length == 2 )
+            {
+                double[] two = new double[] {0, 0};
+                two[0] = vector1[0] + vector2[0];
+                two[1] = vector1[1] + vector2[1];
+                return (two);
+            }
+            if (vector1.Length == 3 && vector2.Length == 3)
+            {
+                double[] three = new double[] {0, 0, 0};
+                three[0] = vector1[0] + vector2[0];
+                three[1] = vector1[1] + vector2[1];
+                three[2] = vector1[2] + vector2[2];
+                return (three);       
+            }
+            else
+            {
+                double[] none = new double[] {-1};
+                return(none);
+            }
+        }
+    }

588fe20fbe50ccd2d02e414fdeefe7d45ae9e072
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-27 12:04:37 -0500

    [Fix]Erase matrix1 and matrix2

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -3,42 +3,40 @@
 ///<summary>Class VectorMath</summary>
     class MatrixMath
     {
-        ///<summary>adds two matrices</summary>
+        ///<summary>multiplies a matrix and a scalar</summary>
         ///<return>returns the resulting matrix</return>
         public static double[,] MultiplyScalar(double[,] matrix, double scalar)
         {
-            if (matrix1.GetLength(0) == 2 && matrix2.GetLength(0) == 2 && 
-            matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2)
+            if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2 )
             {
                 double[,] ans= {
                     {0, 0},
                     {0, 0}
                 };
-                ans[0, 0] = matrix1[0, 0] * scalar;
-                ans[0, 1] = matrix1[0, 1] * scalar;
-                ans[1, 0] = matrix1[1, 0] * scalar;
-                ans[1, 1] = matrix1[1, 1] * scalar;
+                ans[0, 0] = matrix[0, 0] * scalar;
+                ans[0, 1] = matrix[0, 1] * scalar;
+                ans[1, 0] = matrix[1, 0] * scalar;
+                ans[1, 1] = matrix[1, 1] * scalar;
                 return (ans);
             }
-            if (matrix1.GetLength(0) == 3 && matrix2.GetLength(0) == 3 && 
-            matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3)
+            if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
             {
                 double[,] ans= {
                     {0, 0, 0},
                     {0, 0, 0},
                     {0, 0, 0}
                 };
-                ans[0, 0] = matrix1[0, 0] * scalar;
-                ans[0, 1] = matrix1[0, 1] * scalar;
-                ans[0, 2] = matrix1[0, 2] * scalar;
+                ans[0, 0] = matrix[0, 0] * scalar;
+                ans[0, 1] = matrix[0, 1] * scalar;
+                ans[0, 2] = matrix[0, 2] * scalar;
         
-                ans[1, 0] = matrix1[1, 0] * scalar;
-                ans[1, 1] = matrix1[1, 1] * scalar;
-                ans[1, 2] = matrix1[1, 2] * scalar;
+                ans[1, 0] = matrix[1, 0] * scalar;
+                ans[1, 1] = matrix[1, 1] * scalar;
+                ans[1, 2] = matrix[1, 2] * scalar;
                 
-                ans[2, 0] = matrix1[2, 0] * scalar;
-                ans[2, 1] = matrix1[2, 1] * scalar;
-                ans[2, 2] = matrix1[2, 2] * scalar;
+                ans[2, 0] = matrix[2, 0] * scalar;
+                ans[2, 1] = matrix[2, 1] * scalar;
+                ans[2, 2] = matrix[2, 2] * scalar;
 
                 return (ans);       
             }

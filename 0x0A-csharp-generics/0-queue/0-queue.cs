86a70877dfdca5aa7c6febc191afc9a3feee660f
Javier <javier.gutierrez.cruz@gmail.com>
2020-07-27 14:16:01 -0500

    [Fix]xml tags

--- 0x0A-csharp-generics/0-queue/0-queue.cs
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-/// <summary>Queue<T> Generic class </summary>
+/// <summary>Queue Generic class </summary>
     class Queue<T>
     {
         /// <summary>returns type of the object</summary>

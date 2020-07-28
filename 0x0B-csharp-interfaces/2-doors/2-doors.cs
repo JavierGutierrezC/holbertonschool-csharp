813bde5cc2c6c08db13bdbe303bc41f3617bce3d
Javier <javier.gutierrez.cruz@gmail.com>
2020-07-28 13:28:30 -0500

    [Fix]add name variable and initialize name

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary>Class Base</summary>
+public abstract class Base 
+{
+    /// <summary>Public property</summary>
+    public string name;
+    
+    /// <summary>Overides to string method</summary>
+        public override string ToString()
+        {
+            return name + " is a " + GetType();
+        }
+    
+}
+/// <summary>Interface</summary>
+public interface IInteractive
+{
+    /// <summary>method</summary>
+    void Interact();
+}
+/// <summary>Interface</summary>
+public interface IBreakable
+{
+    /// <summary>Properties</summary>
+    int durability
+    { get; set; }
+    /// <summary>method</summary>
+    void Break();
+}
+/// <summary>Interface</summary>
+public interface ICollectible
+{
+    /// <summary>Properties</summary>
+    bool isCollected
+    { get; set; }
+    /// <summary>method</summary>
+    void Collect();
+}
+/// <summary>Class that that inherits from ...</summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>Constructor taht set the value of name</summary>
+    public Door(string door = "Door")
+    {
+        name = door;
+    }
+    /// <summary>method from IInteractive</summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + name +". It's locked.");
+    }
+}

8381966a4fa4a4295bfae25b16fff249f64e0b64
Javier <javier.gutierrez.cruz@gmail.com>
2020-07-29 16:44:28 -0500

    [Fix]grammar and this.name out of else

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// <summary>Class Player</summary>
+public class Player
+{
+    /// <summary>Class Constructor</summary>
+    private string name;
+    private float maxHp;
+    private float hp;
+
+
+    /// <summary>Constructor prototype, assign defaul parameters</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        
+        if (maxHp <= 0f)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;      
+        }
+        this.name = name;
+        this.hp = maxHp;
+    }
+
+    /// <summary>Print Health Method</summary>
+    public void PrintHealth()
+    {
+        //Console.WriteLine("rtyrrete");
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health.");
+    }
+    /// <summary>Delegate takes float as param</summary>
+    public delegate void CalculateHealth(float dam);
+
+    /// <summary>Take damage method. Affect hp</summary>
+    public void TakeDamage(float damage)
+    {
+        Console.WriteLine(this.name + " takes " + damage + " damage!");
+        if (damage < 0f)
+            damage = 0f;
+            this.hp = this.hp - damage;
+        //Console.WriteLine(this.name + " takes 0 damage!");
+
+    }
+
+    /// <summary>Heal damage method. Affect hp</summary>
+    public void HealDamage(float heal)
+    {
+        Console.WriteLine(this.name + " heals " + heal + " HP!"); 
+        if (heal < 0)
+            heal = 0f;
+            this.hp = this.hp + heal;
+        //Console.WriteLine(this.name + " heals 0 damage!");
+    }    
+
+}

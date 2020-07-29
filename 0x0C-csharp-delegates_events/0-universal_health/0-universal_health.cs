2355d2c3e93893c1d8c2859004309a541f965978
Javier <javier.gutierrez.cruz@gmail.com>
2020-07-29 12:49:15 -0500

    [Fix]Use this.

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>Class Player</summary>
+public class Player
+{
+    /// <summary>Class Constructor</summary>
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>Constructor prototype, assign defaul parameters</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        
+        if (maxHp <= 0f)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    /// <summary>Print Health Method</summary>
+    public void PrintHealth()
+    {
+        //Console.WriteLine("rtyrrete");
+        Console.WriteLine(this.name + " has " + this.hp + "/ " + this.maxHp + " health.");
+    }
+}
+

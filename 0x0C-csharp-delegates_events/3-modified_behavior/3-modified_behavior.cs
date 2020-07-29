95cd86e4ffc0257f8fddbe23a06e7416a014cbb0
Javier <javier.gutierrez.cruz@gmail.com>
2020-07-29 14:34:44 -0500

    [Fix]mke enum public

---
+++ 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+public enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+
+/// <summary>Class Player</summary>
+public class Player
+{
+    /// <summary>Class Constructor</summary>
+    private string name;
+    private float maxHp;
+    private float hp;
+
+
+    /// <summary>Constructor prototype, assign defaul parameters</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        
+        if (maxHp <= 0f)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    /// <summary>Print Health Method</summary>
+    public void PrintHealth()
+    {
+        //Console.WriteLine("rtyrrete");
+        Console.WriteLine(this.name + " has " + this.hp + "/ " + this.maxHp + " health.");
+    }
+
+    /// <summary>Take damage method. Affect hp</summary>
+    public void TakeDamage(float damage)
+    {
+        float newHp = this.hp;
+        Console.WriteLine(this.name + " takes " + damage + " damage!");
+        //Console.WriteLine("newhp " + newHp);
+        //Console.WriteLine("damage " + damage);
+        if (damage < 0f)
+            damage = 0f;     
+        newHp = newHp - damage;
+        //Console.WriteLine("newHpafterdmg " + newHp);
+            ValidateHP(newHp);
+    }
+
+    /// <summary>Heal damage method. Affect hp</summary>
+    public void HealDamage(float heal)
+    {
+        float newHp = this.hp;
+        Console.WriteLine(this.name + " heals " + heal + " HP!"); 
+        if (heal < 0f)
+            heal = 0f;
+        newHp = newHp + heal;
+            ValidateHP(newHp);
+    }  
+
+    /// <summary>Sets new value of players hp</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+            this.hp = 0f;
+        if (newHp > this.maxHp)
+            this.hp = maxHp;
+        else
+            this.hp = newHp;
+    }
+
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Weak)
+        {
+            return (baseValue / 2f);
+        }
+        else if (modifier == Modifier.Base)
+        {
+            return (baseValue);
+        }
+        else
+        {
+            return (baseValue * 1.5f);
+        }
+    }
+}
+
+/// <summary>Delegate takes float and the enum modifiers as param</summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
